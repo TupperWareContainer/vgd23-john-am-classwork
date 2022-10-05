1a3a53a97748add9098a5b0007e1c69d7eeed0db
VGD_2022x23_John_Fotheringham <1713323@mukilteo.wednet.edu>
2022-10-05 10:24:46 -0700

    also this thing

--- Game Backups/Unity Projects/Triple SMP Submission/Assets/Scripts/Aim.cs
+++ Game Backups/Unity Projects/Triple SMP Submission/Assets/Scripts/Aim.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Aim : MonoBehaviour
 {
     private Camera cam;
-    public Vector2 mousePos;
+    public Vector3 mousePos;
     public Transform debugObj; 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,9 @@ public class Aim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,0f));
+        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,cam.nearClipPlane));
+        Debug.Log($" screen to world point: { cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane))}");
+        Debug.Log($"mouseposx {Input.mousePosition.x}"); 
         transform.position = mousePos; 
     }
     
