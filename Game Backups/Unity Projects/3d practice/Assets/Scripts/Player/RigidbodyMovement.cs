653f8766b663ac58f6b2fdaaff068bc8f5a31374
JohnF <boxtrotterror@gmail.com>
2022-11-09 14:44:16 -0800

    added some stuff to the 3d test scene and imported my personal project

--- Game Backups/Unity Projects/3d practice/Assets/Scripts/Player/RigidbodyMovement.cs
+++ Game Backups/Unity Projects/3d practice/Assets/Scripts/Player/RigidbodyMovement.cs
@@ -6,12 +6,36 @@ public class RigidbodyMovement : MonoBehaviour
 {
     private Rigidbody rb;
     public float moveMult = 3f;
-
+    [SerializeField] private bool forward = false; 
     private void Start()
     {
         rb = GetComponent<Rigidbody>(); 
     }
-    
+    // update is used to get input 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) forward = true;
+        else forward = false; 
+    }
+    private void MovePlayer(bool forward,bool up)
+    {
+        Vector3 mVector;
+        float dX,dY,dZ = 0f;
+
+
+        if (forward) dX = moveMult * Time.fixedDeltaTime;
+        else dX = 0f;
+        if (up) dY = moveMult * Time.fixedDeltaTime;
+        else dY = rb.velocity.y;
+        mVector = new Vector3(dX, dY, dZ);
+        rb.velocity = mVector; 
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer(forward, false); 
+    }
+
+
 
 
 }
